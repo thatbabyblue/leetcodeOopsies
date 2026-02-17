f53b61b6084b7e5b21f404b3790833f04d068834
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-16 20:04:00 -0800

    Add solution: 107. Binary Tree Level Order Traversal II

---
+++ 107. Binary Tree Level Order Traversal II/107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,41 @@
+/*
+ * Problem: 107. Binary Tree Level Order Traversal II
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ * Language: cpp
+ * Date: 2026-02-17
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeNode*> que;
+        vector<vector<int>> res;
+        if (root != nullptr) que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            vector<int> vec;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                vec.push_back(node->val);
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+            res.push_back(vec);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};

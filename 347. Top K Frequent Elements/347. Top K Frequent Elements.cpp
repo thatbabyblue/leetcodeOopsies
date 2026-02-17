29223867f15c5fa63c55452b99bdfd80678edaa7
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-16 18:49:40 -0800

    Add solution: 347. Top K Frequent Elements

---
+++ 347. Top K Frequent Elements/347. Top K Frequent Elements.cpp
@@ -0,0 +1,38 @@
+/*
+ * Problem: 347. Top K Frequent Elements
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/top-k-frequent-elements/description/
+ * Language: cpp
+ * Date: 2026-02-17
+ */
+
+class Solution {
+public:
+    class comp {
+    public:
+        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+            return lhs.second > rhs.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pri_que;
+        
+        for (unordered_map<int, int>::iterator it = map.begin(); it != map.end(); it++) {
+            pri_que.push(*it);
+            if (pri_que.size() > k) {
+                pri_que.pop();
+            }
+        }
+        vector<int> res(k);
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = pri_que.top().first;
+            pri_que.pop();
+        }
+        return res;
+    }
+};

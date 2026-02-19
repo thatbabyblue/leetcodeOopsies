473686996e68588c886e4350686e39430bef1e83
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-18 21:20:14 -0800

    Update solution: 617. Merge Two Binary Trees

--- 617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
+++ 617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 617. Merge Two Binary Trees
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/merge-two-binary-trees/submissions/1923978077/
+ * Link: https://leetcode.com/problems/merge-two-binary-trees/submissions/1923980393/
  * Language: cpp
  * Date: 2026-02-19
  */
@@ -20,14 +20,11 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if (root1 == nullptr && root2 == nullptr) return nullptr;
-        int rootVal;
         if (root1 == nullptr) return root2;
-        else if (root2 == nullptr) return root1;
-        else rootVal = root1->val + root2->val;
-        TreeNode* node = new TreeNode(rootVal);
-        node->left = mergeTrees(root1->left, root2->left);
-        node->right = mergeTrees(root1->right, root2->right);
-        return node;
+        if (root2 == nullptr) return root1;
+        root1->val += root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        return root1;
     }
 };

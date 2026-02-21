1d965a13e3fb264ea72a57c06d75a9cab6947127
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-20 19:10:38 -0800

    Add solution: 1049. Last Stone Weight II

---
+++ 1049. Last Stone Weight II/1049. Last Stone Weight II.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem: 1049. Last Stone Weight II
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/last-stone-weight-ii/submissions/1925883176/
+ * Language: cpp
+ * Date: 2026-02-21
+ */
+
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        vector<int> dp(15001, 0);
+        int sum = 0;
+        for (int i = 0; i < stones.size(); i++) {
+            sum += stones[i];
+        }
+        int target = sum / 2;
+        for (int i = 0; i < stones.size(); i++) {
+            for (int j = target; j >= stones[i]; j--) {
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        return sum - dp[target] - dp[target];
+    }
+};

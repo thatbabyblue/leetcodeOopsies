2afc762d81fdbb1b196ea3704a95a5574ce9fbec
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-22 11:37:53 -0800

    Add solution: 1143. Longest Common Subsequence

---
+++ 1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: 1143. Longest Common Subsequence
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/longest-common-subsequence/submissions/1927784703/
+ * Language: cpp
+ * Date: 2026-02-22
+ */
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+        int res = 0;
+        for (int i = 1; i <= text1.size(); i++) {
+            for (int j = 1; j <= text2.size(); j++) {
+                if (text1[i - 1] == text2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                if (dp[i][j] > res) res = dp[i][j];
+            }
+        }
+        return res;
+
+    }
+};

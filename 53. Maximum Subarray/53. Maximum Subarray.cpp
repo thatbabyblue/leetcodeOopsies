ad4c7653c473a9b8aac8cf497dcffebe8834612a
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-23 21:10:14 -0800

    Update solution: 53. Maximum Subarray

--- 53. Maximum Subarray/53. Maximum Subarray.cpp
+++ 53. Maximum Subarray/53. Maximum Subarray.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 53. Maximum Subarray
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/maximum-subarray/submissions/1929238582/
+ * Link: https://leetcode.com/problems/maximum-subarray/submissions/1929247716/
  * Language: cpp
  * Date: 2026-02-24
  */
@@ -9,12 +9,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        int res = dp[0];
-        for (int i = 1; i < nums.size(); i++) {
-            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
-            if (dp[i] > res) res = dp[i];
+        int res =  INT_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            count += nums[i];
+            if (count > res) res = count;
+            if (count <= 0) count = 0;
         }
         return res;
     }

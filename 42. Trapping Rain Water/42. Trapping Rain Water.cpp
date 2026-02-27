b25fad6bdbd388b4f432f4ede028efb73e48f89d
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-26 20:59:57 -0800

    Update solution: 42. Trapping Rain Water

--- 42. Trapping Rain Water/42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water/42. Trapping Rain Water.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 42. Trapping Rain Water
  * Difficulty: Hard
- * Link: https://leetcode.com/problems/trapping-rain-water/submissions/1932416373/
+ * Link: https://leetcode.com/problems/trapping-rain-water/submissions/1932429850/
  * Language: cpp
  * Date: 2026-02-27
  */
@@ -11,24 +11,26 @@ public:
     int trap(vector<int>& height) {
         if (height.size() <= 2) return 0;
         int sum = 0;
-        int size = height.size();
-        vector<int> maxLeft(size, 0);
-        vector<int> maxRight(size, 0);
-
-        maxLeft[0] = height[0];
-        for (int i = 1; i < size; i++) {
-            maxLeft[i] = max(height[i], maxLeft[i - 1]);
-        }
-
-        maxRight[size - 1] = height[size - 1];
-        for (int j = size - 2; j >= 0; j--) {
-            maxRight[j] = max(height[j], maxRight[j + 1]);
-        }
-        for (int i = 0; i < size; i++) {
-            int cnt = min(maxLeft[i], maxRight[i]) - height[i];
-            if (cnt > 0) sum += cnt;
+        stack<int> st;
+        st.push(0);
+        for (int i = 1; i < height.size(); i++) {
+            if (height[i] < height[st.top()]) st.push(i);
+            else if (height[i] == height[st.top()]) {
+                st.pop();
+                st.push(i);
+            } else {
+                while (!st.empty() && height[i] > height[st.top()]) {
+                    int mid = st.top();
+                    st.pop();
+                    if (!st.empty()) {
+                        int h = min(height[st.top()], height[i]) - height[mid];
+                        int w = i - st.top() - 1;
+                        sum += h * w;
+                    }
+                }
+                st.push(i);
+            }
         }
         return sum;
-
     }
 };

583c7bee745dcb86fe4ecf901e9a0e1e25afe498
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-28 10:26:58 -0800

    Update solution: 130. Surrounded Regions

--- 130. Surrounded Regions/130. Surrounded Regions.cpp
+++ 130. Surrounded Regions/130. Surrounded Regions.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 130. Surrounded Regions
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/surrounded-regions/submissions/1933984397/
+ * Link: https://leetcode.com/problems/surrounded-regions/submissions/1933997910/
  * Language: cpp
  * Date: 2026-02-28
  */
@@ -9,25 +9,35 @@
 class Solution {
 public:
     int dir[4][2] = {1, 0, 0, -1, -1, 0, 0, 1};
-    void dfs(vector<vector<char>>& board, int x, int y) {
+    void bfs(vector<vector<char>>& board, int x, int y) {
+        queue<pair<int, int>> que;
         board[x][y] = 'y';
-        for (int i = 0; i < 4; i++) {
-            int nextx = x + dir[i][0];
-            int nexty = y + dir[i][1];
-            if (nextx < 0 || nextx >= board.size() || nexty < 0 || nexty >= board[0].size()) continue;
-            if (board[nextx][nexty] == 'O') dfs(board, nextx, nexty);
+        que.push({x, y});
+        while (!que.empty()) {
+            int curx = que.front().first;
+            int cury = que.front().second;
+            que.pop();
+            for (int i = 0; i < 4; i++) {
+                int nextx = curx + dir[i][0];
+                int nexty = cury + dir[i][1];
+                if (nextx < 0 || nextx >= board.size() || nextx < 0 || nexty >= board[0].size()) continue;
+                if (board[nextx][nexty] == 'O') {
+                    board[nextx][nexty] = 'y';
+                    que.push({nextx, nexty});
+                }
+            }
         }
     }
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
         for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') dfs(board, i, 0);
-            if (board[i][m - 1] == 'O') dfs(board, i, m - 1);
+            if (board[i][0] == 'O') bfs(board, i, 0);
+            if (board[i][m - 1] == 'O') bfs(board, i, m - 1);
         }
         for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O') dfs(board, 0, j);
-            if (board[n - 1][j] == 'O') dfs(board, n - 1, j);
+            if (board[0][j] == 'O') bfs(board, 0, j);
+            if (board[n - 1][j] == 'O') bfs(board, n - 1, j);
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {

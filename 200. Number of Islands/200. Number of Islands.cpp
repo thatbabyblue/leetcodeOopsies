8caac2c9dd4d2e0815335c7b5efdf6caa205a26d
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-27 21:00:53 -0800

    Update solution: 200. Number of Islands

--- 200. Number of Islands/200. Number of Islands.cpp
+++ 200. Number of Islands/200. Number of Islands.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 200. Number of Islands
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/number-of-islands/submissions/1933279037/
+ * Link: https://leetcode.com/problems/number-of-islands/submissions/1933289248/
  * Language: cpp
  * Date: 2026-02-28
  */
@@ -9,14 +9,23 @@
 class Solution {
 public:
     int dir[4][2] = {0, 1, 1, 0, 0, -1, -1, 0};
-    void dfs(const vector<vector<char>>& grid, vector<vector<bool>>& visited, int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nextx = x + dir[i][0];
-            int nexty = y + dir[i][1];
-            if (nextx < 0 || nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) continue;
-            if (!visited[nextx][nexty] && grid[nextx][nexty] == '1') {
-                visited[nextx][nexty] = true;
-                dfs(grid, visited, nextx, nexty);
+    void bfs(const vector<vector<char>>& grid, vector<vector<bool>>& visited, int x, int y) {
+        queue<pair<int, int>> que;
+        que.push({x, y});
+        visited[x][y] = true;
+        while (!que.empty()) {
+            pair<int, int> cur = que.front();
+            que.pop();
+            int curx = cur.first;
+            int cury = cur.second;
+            for (int i = 0; i < 4; i++) {
+                int nextx = curx + dir[i][0];
+                int nexty = cury + dir[i][1];
+                if (nextx < 0 || nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) continue;
+                if (!visited[nextx][nexty] && grid[nextx][nexty] == '1') {
+                    que.push({nextx, nexty});
+                    visited[nextx][nexty] = true;
+                }
             }
         }
     }
@@ -29,8 +38,7 @@ public:
             for (int j = 0; j < m; j++) {
                 if (!visited[i][j] && grid[i][j] == '1') {
                     res++;
-                    visited[i][j] = true;
-                    dfs(grid, visited, i, j);
+                    bfs(grid, visited, i, j);
                 }
             }
         }

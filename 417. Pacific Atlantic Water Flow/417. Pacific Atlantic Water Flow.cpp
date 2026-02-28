aa540e34c8832181dbf15656fa1dbd92fbf3e7a6
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-02-28 12:10:17 -0800

    Update solution: 417. Pacific Atlantic Water Flow

--- 417. Pacific Atlantic Water Flow/417. Pacific Atlantic Water Flow.cpp
+++ 417. Pacific Atlantic Water Flow/417. Pacific Atlantic Water Flow.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 417. Pacific Atlantic Water Flow
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/pacific-atlantic-water-flow/submissions/1934056120/
+ * Link: https://leetcode.com/problems/pacific-atlantic-water-flow/submissions/1934064128/
  * Language: cpp
  * Date: 2026-02-28
  */
@@ -9,16 +9,25 @@
 class Solution {
 public:
     int dir[4][2] = {-1, 0, 0, -1, 1, 0, 0, 1};
-    void dfs(const vector<vector<int>>& heights, vector<vector<bool>>& visited, int x, int y) {
+    void bfs(const vector<vector<int>>& heights, vector<vector<bool>>& visited, int x, int y) {
         if (visited[x][y]) return;
         visited[x][y] = true;
-        
-        for (int i = 0; i < 4; i++) {
-            int nextx = x + dir[i][0];
-            int nexty = y + dir[i][1];
-            if (nextx < 0 || nextx >= heights.size() || nexty < 0 || nexty >= heights[0].size()) continue;
-            if (heights[x][y] > heights[nextx][nexty]) continue;
-            dfs(heights, visited, nextx, nexty);
+        queue<pair<int, int>> que;
+        que.push({x, y});
+        while (!que.empty()) {
+            int curx = que.front().first;
+            int cury = que.front().second;
+            que.pop();
+            for (int i = 0; i < 4; i++) {
+                int nextx = curx + dir[i][0];
+                int nexty = cury + dir[i][1];
+                if (nextx < 0 || nextx >= heights.size() || nexty < 0 || nexty >= heights[0].size()) continue;
+                if (visited[nextx][nexty]) continue;
+                if (heights[curx][cury] <= heights[nextx][nexty]) {
+                    visited[nextx][nexty] = true;
+                    que.push({nextx, nexty});
+                }
+            }
         }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
@@ -28,12 +37,12 @@ public:
         vector<vector<bool>> firstBorder(n, vector<bool>(m, false));
         vector<vector<bool>> secondBorder(n, vector<bool>(m, false));
         for (int i = 0; i < n; i++) {
-            dfs(heights, firstBorder, i, 0);
-            dfs(heights, secondBorder, i, m - 1);
+            bfs(heights, firstBorder, i, 0);
+            bfs(heights, secondBorder, i, m - 1);
         }
         for (int j = 0; j < m; j++) {
-            dfs(heights, firstBorder, 0, j);
-            dfs(heights, secondBorder, n - 1, j);
+            bfs(heights, firstBorder, 0, j);
+            bfs(heights, secondBorder, n - 1, j);
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {

556b3ec27ad1f446128c61bb8030197d7d479b4f
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-03-08 13:53:34 -0700

    Update solution: 283. Move Zeroes

--- 283. Move Zeroes/283. Move Zeroes.cpp
+++ 283. Move Zeroes/283. Move Zeroes.cpp
@@ -1,22 +1,20 @@
 /*
  * Problem: 283. Move Zeroes
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/move-zeroes/submissions/1925948316/
+ * Link: https://leetcode.com/problems/move-zeroes/submissions/1942283687/
  * Language: cpp
- * Date: 2026-02-21
+ * Date: 2026-03-08
  */
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int slow = 0, fast = 0;
-        for (; fast < nums.size(); fast++) {
-            if (nums[fast] != 0) {
-                nums[slow++] = nums[fast];
-            }
+        int slow = 0;
+        for (int fast = 0; fast < nums.size(); fast++) {
+            if (nums[fast] != 0) nums[slow++] = nums[fast];
         }
-        for (; slow < nums.size(); slow++) {
-            nums[slow] = 0;
+        for (int i = slow; i < nums.size(); i++) {
+            nums[i] = 0;
         }
     }
 };

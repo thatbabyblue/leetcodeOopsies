ec161270850db585814c5eca1e4e8fd296eb0d4c
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-03-14 15:10:18 -0700

    Update solution: 541. Reverse String II

--- 541. Reverse String II/541. Reverse String II.cpp
+++ 541. Reverse String II/541. Reverse String II.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 541. Reverse String II
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/reverse-string-ii/submissions/1948432448/
+ * Link: https://leetcode.com/problems/reverse-string-ii/submissions/1948432961/
  * Language: cpp
  * Date: 2026-03-14
  */
@@ -11,9 +11,9 @@ public:
     string reverseStr(string s, int k) {
         for (int i = 0; i < s.size(); i += 2 * k) {
             if (i + k < s.size()) {
-                reverse(s.begin(), s.begin() + i + k);
+                reverse(s.begin() + i, s.begin() + i + k);
             }else {
-                reverse(s.begin(), s.end());
+                reverse(s.begin() + i, s.end());
             }
         }
         return s;

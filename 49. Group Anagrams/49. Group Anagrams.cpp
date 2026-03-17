31d16760849f94de3893565ca29d4eb30b873f51
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-03-16 21:32:33 -0700

    Update solution: 49. Group Anagrams

--- 49. Group Anagrams/49. Group Anagrams.cpp
+++ 49. Group Anagrams/49. Group Anagrams.cpp
@@ -1,24 +1,22 @@
 /*
  * Problem: 49. Group Anagrams
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/group-anagrams/submissions/1947561236/
+ * Link: https://leetcode.com/problems/group-anagrams/
  * Language: cpp
- * Date: 2026-03-14
+ * Date: 2026-03-17
  */
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
         vector<vector<string>> res;
-        unordered_map<string, vector<string>> umap;
-        for (auto& str : strs) {
+        for (string& str : strs) {
             string count(26, 0);
-            for (auto& c : str) {
-                count[c - 'a']++;
-            }
-            umap[count].push_back(str);
+            for (auto c : str) count[c - 'a']++;
+            map[count].push_back(str);
         }
-        for (auto& pair : umap) {
+        for (auto& pair : map) {
             res.push_back(pair.second);
         }
         return res;

fe499e96ea4d75ca89d85040dbe58ecb8c4e55fa
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-03-17 08:41:41 -0700

    Update solution: 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
@@ -1,22 +1,20 @@
 /*
  * Problem: 121. Best Time to Buy and Sell Stock
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1926964217/
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1951241841/
  * Language: cpp
- * Date: 2026-02-22
+ * Date: 2026-03-17
  */
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int len = prices.size();
-        vector<vector<int>> dp(len, vector<int>(2));
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
-        for (int i = 1; i < len; i++) {
-            dp[i][0] = max(dp[i - 1][0], -prices[i]);
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        int min_price = INT_MAX;
+        int max_profit = INT_MIN;
+        for (int price : prices) {
+            min_price = min(min_price, price);
+            max_profit = max(max_profit, price - min_price);
         }
-        return dp[len - 1][1];
+        return max_profit;
     }
 };

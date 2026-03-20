860c83429952eab5bca0d6f07261f90b353068bb
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-03-19 23:50:05 -0700

    Add solution: 11. Container With Most Water

---
+++ 11. Container With Most Water/11. Container With Most Water.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem: 11. Container With Most Water
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/container-with-most-water/
+ * Language: cpp
+ * Date: 2026-03-20
+ */
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int res = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                res = max(res, height[left] * (right - left));
+                left++;
+            } else {
+                res = max(res, height[right] * (right - left));
+                right--;
+            }
+        }
+        return res;
+    }
+};

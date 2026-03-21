f71ec26230114cabb4f57317e5fe4e18c14e1122
thatbabyblue <109011956+thatbabyblue@users.noreply.github.com>
2026-03-21 12:20:03 -0700

    Update solution: 257. Binary Tree Paths

--- 257. Binary Tree Paths/257. Binary Tree Paths.cpp
+++ 257. Binary Tree Paths/257. Binary Tree Paths.cpp
@@ -1,9 +1,9 @@
 /*
  * Problem: 257. Binary Tree Paths
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/binary-tree-paths/submissions/1922970645/
+ * Link: https://leetcode.com/problems/binary-tree-paths/submissions/1955067741/
  * Language: cpp
- * Date: 2026-02-18
+ * Date: 2026-03-21
  */
 
 /**
@@ -18,34 +18,32 @@
  * };
  */
 class Solution {
-public:
-    void traversal(TreeNode* node, vector<int>& path, vector<string>& res) {
-        path.push_back(node->val);
-        if (node->left == nullptr && node->right == nullptr) {
-            string spath;
+public: 
+    void traversal(TreeNode* cur, vector<string>& res, vector<int>& path) {
+        path.push_back(cur->val);
+        if (cur->left == nullptr && cur->right == nullptr) {
+            string s;
             for (int i = 0; i < path.size() - 1; i++) {
-                spath += to_string(path[i]);
-                spath += "->";
+                s += to_string(path[i]);
+                s += "->";
             }
-            spath += to_string(path[path.size() - 1]);
-            res.push_back(spath);
+            s += to_string(path[path.size() - 1]);
+            res.push_back(s);
             return;
         }
-
-        if (node->left) {
-            traversal(node->left, path, res);
+        if (cur->left) {
+            traversal(cur->left, res, path);
             path.pop_back();
         }
-        if (node->right) {
-            traversal(node->right, path, res);
+        if (cur->right) {
+            traversal(cur->right, res, path);
             path.pop_back();
         }
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> res;
         vector<int> path;
-        if (root == nullptr) return res;
-        traversal(root, path, res);
+        traversal(root, res, path);
         return res;
     }
 };
